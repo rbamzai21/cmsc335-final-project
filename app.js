8640eb3dcd08f3a926b38790f3f2e9e42af81e5f
rbamzai21 <rishab.bamzai@gmail.com>
2025-12-15 00:55:03 -0500

    mongo integration

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.set("view engine", "ejs");
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+  .then(() => console.log("Connected"))
+  .catch(err => console.error(err));
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
--- routes/routing_events.js
+++ routes/routing_events.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+const eventSchema = new mongoose.Schema({
+  name: { type: String, required: true }, 
+  date: { type: Date, required: true}
+}, { timestamps: true });
+
+const Event = mongoose.model("Event", eventSchema);
+
+router.get("/", async(req, res) => {
   res.render("index");
 });
 
@@ -9,12 +17,33 @@ router.get("/new", (req, res) => {
   res.render("new");
 });
 
-router.get("/events", (req, res) => {
-  res.render("events");
+router.post("/new", async (req, res) => {
+  try {
+    const { name, date } = req.body;
+    const newEvent = new Event({ name, date });
+    await newEvent.save();
+  } catch {
+    res.status(400).send(err.message);
+  }
 });
 
-router.get("/clear", (req, res) => {
+router.get("/events", async(req, res) => {
+  try {
+    const events = await Event.find().sort({ date: 1 });
+    res.render("events", { events });
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+router.post("/clear", async (req, res) => {
   res.render("clear");
+  try {
+    await Event.deleteMany({});res
+  } catch {
+    res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
